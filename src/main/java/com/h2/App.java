e8603d19eecf8c42cbc40205537ac63a623d9603
lilysashad <lilysdeng@gmail.com>
2022-08-03 15:19:44 -0400

    Finding best loan rates (1)

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,12 +6,23 @@ package com.h2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        int[] array = {1,2,3,4};
+        int num = 2;
+        System.out.println(add(array));
+        System.out.println(doubleTheNumber(num));
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int num : numbers){
+            sum += num;
+        }
+        return sum;
     }
 }
